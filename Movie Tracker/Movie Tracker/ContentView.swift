04ba8acfe133b7042d66b484a4159f68531ad0a1
Alexandre C do Carmo <alexandrecdocarmo@Mac-mini-de-Alexandre-2.local>
2022-05-17 16:07:44 -0300

    estrutura inicial do projeto

---
+++ Movie Tracker/Movie Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie Tracker
+//
+//  Created by Alexandre C do Carmo on 17/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
