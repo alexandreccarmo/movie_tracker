ae5d44c828e44346e2f03dcd9ee4e319e09109bd
Alexandre C do Carmo <alexandrecdocarmo@Mac-mini-de-Alexandre-2.local>
2022-05-18 15:02:25 -0300

    form com rating, textfield, list. sections, etc

--- Movier Tracker/Movier Tracker/ContentView.swift
+++ Movier Tracker/Movier Tracker/ContentView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var title = ""
+    @State var rating = 3.0
+    @State var seen = false
+    
     var body: some View {
 //        Image("code").resizable().aspectRatio(contentMode: .fit)
        
@@ -60,12 +65,53 @@ struct ContentView: View {
 //            }
 //        }.padding().background(Color.blue).cornerRadius(10).foregroundColor(.white)
         
-        Button(action: {}){
-            VStack {
-                Image(systemName: "gamecontroller")
-                Text("Tap Me")
+//        Button(action: {}){
+//            VStack {
+//                Image(systemName: "gamecontroller")
+//                Text("Tap Me")
+//            }
+//        }.padding().background(Color.blue).cornerRadius(10).foregroundColor(.white)
+        
+        List {
+            
+            Section{
+                SectionTitle(title: "Title")
+                TextField("Movie Title",text: $title)
             }
-        }.padding().background(Color.blue).cornerRadius(10).foregroundColor(.white)
+            
+            Section{
+                SectionTitle(title: "Rating")
+                HStack{
+                    Spacer()
+                    Text(String(repeating:"★", count:  Int(rating))).foregroundColor(.yellow).font(.title)
+                    Spacer()
+                }
+                Slider(value:$rating, in: 1...5, step: 1)
+            }
+            
+            Section {
+                SectionTitle(title: "Seen")
+                Toggle(isOn:$seen, label: {
+                    if title == "" {
+                        Text("I have seen this movie")
+                    }
+                    else{
+                        Text("I have seen \(title)")
+                    }
+                    
+                })
+            }
+            Section {
+                Button(action:{}) {
+                    HStack {
+                        Spacer()
+                        Text("Save")
+                        Spacer()
+                    }
+                }
+            }
+            
+        }.listStyle(GroupedListStyle())
        
         
     }
@@ -78,5 +124,14 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SectionTitle: View {
+    
+    var title: String
+    
+    var body: some View {
+        Text(title).font(.caption).foregroundColor(.gray)
     }
 }
